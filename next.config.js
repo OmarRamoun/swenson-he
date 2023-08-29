041b0bf4c6c026704fc6818f4bca3f7153eeeafd
Omar Ramoun <contact@omarramoun.com>
2023-08-29 20:56:45 +0300

    build(configs): add configs for typescript and eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'swensonhe-dev-challenge.s3.us-west-2.amazonaws.com',
+        port: '',
+      },
+    ],
+  },
 
-module.exports = nextConfig
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [{loader: '@svgr/webpack', options: {icon: true}}],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
