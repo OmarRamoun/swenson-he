b9349fe05bb19034baace3b0607c76a0e82c61d3
Omar Ramoun <contact@omarramoun.com>
2023-08-29 20:36:35 +0300

    refactor(assets): final touches

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,6 @@
+export {default as Arrow} from './arrow.svg';
+export {default as LeftArrow} from './leftArrow.svg';
+export {default as Check} from './check.svg';
+export {default as Cross} from './cross.svg';
+export {default as Plus} from './plus.svg';
+export {default as Star} from './star.svg';
