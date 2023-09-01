900c78b5be94e5ccf67a05b3308e07d7eb7cd87a
Omar Ramoun <contact@omarramoun.com>
2023-09-01 18:36:32 +0300

    refactor(utils): solve ssr issue with window obj

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,11 +1,23 @@
 const setItem = (key: string, value: string | null): void => {
+  if (typeof window === 'undefined') {
+    // Handle the case when window is not available, e.g., during server-side rendering.
+    return;
+  }
+
+  /* eslint-disable-next-line */
+  const localStorage = window.localStorage;
+
   if (!value) {
-    window.localStorage.removeItem(key);
+    localStorage.removeItem(key);
   } else {
-    window.localStorage.setItem(key, value);
+    localStorage.setItem(key, value);
   }
 };
 
-const getItem = (key: string): string | null => window.localStorage.getItem(key);
+const getItem = (key: string): string | void => {
+  if (typeof window !== 'undefined') {
+    window.localStorage.getItem(key);
+  }
+};
 
 export {getItem, setItem};
