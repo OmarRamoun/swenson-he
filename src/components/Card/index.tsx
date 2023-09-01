d8b47d24944257d825ca7359a77177161890e285
Omar Ramoun <contact@omarramoun.com>
2023-09-01 16:17:31 +0300

    refactor(components): update card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -20,7 +20,7 @@ const Card: FC<CardProps> = ({children, className, src, icon, ...props}) => (
   <Flex className={cn(styles.card, className)} {...props}>
     <Center className={styles.icon_wrapper}>{icon ?? null}</Center>
 
-    <Image className={styles.image} src={src} alt="card thumbnail" width="162" height="104" sizes="16rem" />
+    <Image className={styles.image} src={src} alt="card thumbnail" width="162" height="104" sizes="16rem" priority />
 
     {children}
   </Flex>
