8af5edefd37aca16211cdfb7d9411782e9974b02
Omar Ramoun <contact@omarramoun.com>
2023-09-03 00:39:32 +0300

    refactor(layouts): add transition between pages

--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -1,5 +1,7 @@
 import type {FC} from 'react';
 
+import {usePathname} from 'next/navigation';
+
 import {Flex} from '@components';
 
 import {Nav} from './components/Nav';
@@ -9,11 +11,22 @@ interface HomeLayoutProps {
   children: React.ReactNode;
 }
 
-const HomeLayout: FC<HomeLayoutProps> = ({children}) => (
-  <Flex className={styles.container}>
-    <Nav />
-    {children}
-  </Flex>
-);
+const HomeLayout: FC<HomeLayoutProps> = ({children}) => {
+  const router = usePathname();
+
+  return (
+    <Flex
+      key={router}
+      className={styles.container}
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      exit={{opacity: 0}}
+      transition={{duration: 0.75}}
+    >
+      <Nav />
+      {children}
+    </Flex>
+  );
+};
 
 export {HomeLayout};
